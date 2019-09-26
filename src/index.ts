cfdec59a97dbd7ac538c77ce65a185c8014bef85
Lyn Ho <lyn1107130806@gmail.com>
2019-09-26 17:10:18 +0800

    feat: transform response
    transform response data, headers, responseType and write demo for it.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { AxiosRequestConfig } from './typings'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './typings'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/headers'
 
-function axios(config: AxiosRequestConfig): void {
+function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
 
-  xhr(config)
+  return xhr(config).then(res => transformResponseData(res))
 }
 
 function processConfig(config: AxiosRequestConfig): void {
@@ -30,6 +30,12 @@ function transformHeaders(config: AxiosRequestConfig): any {
   const { headers = {}, data } = config
 
   return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+
+  return res
 }
 
 export default axios

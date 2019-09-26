b78c1e91123978c1d4df369f8f539f1f9b051c6c
Lyn Ho <lyn1107130806@gmail.com>
2019-09-27 01:29:03 +0800

    feat(config): static function create
    add static function create to create an axios instance and demo to test

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -15,7 +15,29 @@ axios.defaults.headers.common['test2'] = 123
 // }).then(res => console.log(res))
 
 // tslint:disable-next-line: no-floating-promises
-axios({
+// axios({
+//   transformRequest: [
+//     function(data) {
+//       return qs.stringify(data)
+//     },
+//     ...(axios.defaults.transformRequest as AxiosTransformer[])
+//   ],
+//   transformResponse: [
+//     ...(axios.defaults.transformResponse as AxiosTransformer[]),
+//     function(data) {
+//       if (typeof data === 'object') {
+//         data.b = 2
+//       }
+//       return data
+//     }
+//   ],
+
+//   url: '/config/post',
+//   method: 'post',
+//   data: { a: 1 }
+// }).then(res => console.log(res.data))
+
+const instance = axios.create({
   transformRequest: [
     function(data) {
       return qs.stringify(data)
@@ -30,9 +52,12 @@ axios({
       }
       return data
     }
-  ],
+  ]
+})
 
+// tslint:disable-next-line: no-floating-promises
+instance({
   url: '/config/post',
   method: 'post',
-  data: { a: 1 }
+  data: { a: 3 }
 }).then(res => console.log(res.data))

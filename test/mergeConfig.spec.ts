f7d3634a0c0c8c42b716fc796a6d81869c37a8c6
Lyn Ho <lyn1107130806@gmail.com>
2019-09-29 13:46:20 +0800

    feat(test): test mergeConfig
    test mergeConfig

---
+++ test/mergeConfig.spec.ts
@@ -0,0 +1,82 @@
+import axios from '../src'
+import mergeConfig from '../src/core/mergeConfig'
+
+describe('mergeConfig', () => {
+  const defaults = axios.defaults
+
+  test('should accept undefined form second argument', () => {
+    expect(mergeConfig(defaults, undefined)).toEqual(defaults)
+  })
+
+  test('should accept an object for second argument', () => {
+    expect(mergeConfig(defaults, {})).toEqual(defaults)
+  })
+
+  test('should not leave references', () => {
+    const merged = mergeConfig(defaults, {})
+
+    expect(merged).not.toBe(defaults)
+    expect(merged.headers).not.toBe(defaults.headers)
+  })
+
+  test('should allow setting request options', () => {
+    const config = {
+      url: '__sample url__',
+      params: '__sample params__',
+      data: { foo: true }
+    }
+
+    const merged = mergeConfig(defaults, config)
+
+    expect(merged.url).toBe(config.url)
+    expect(merged.params).toBe(config.params)
+    expect(merged.data).toEqual(config.data)
+  })
+
+  test('should not inherit request options', () => {
+    const config = {
+      url: '__sample url__',
+      params: '__sample params__',
+      data: { foo: true }
+    }
+
+    const merged = mergeConfig(config, {})
+
+    expect(merged.url).toBeUndefined()
+    expect(merged.params).toBeUndefined()
+    expect(merged.data).toBeUndefined()
+  })
+
+  test('should return default headers if pass config2 with undefined', () => {
+    expect(mergeConfig({ headers: 'x-mock-header' }, undefined)).toEqual({
+      headers: 'x-mock-header'
+    })
+  })
+
+  test('should merge auth, headers with defaults', () => {
+    expect(
+      mergeConfig({ auth: undefined }, { auth: { username: 'foo', password: 'bar' } })
+    ).toEqual({ auth: { username: 'foo', password: 'bar' } })
+
+    expect(
+      mergeConfig(
+        { auth: { username: 'foo', password: 'bar' } },
+        { auth: { username: 'tom', password: 'cat' } }
+      )
+    ).toEqual({ auth: { username: 'tom', password: 'cat' } })
+  })
+
+  test('should overwrite auth, headers with a non-object value', () => {
+    expect(
+      mergeConfig(
+        { headers: { common: { Accept: 'application/json,text/plain,*/*' } } },
+        { headers: null }
+      )
+    ).toEqual({ headers: null })
+  })
+
+  test('should allow setting other options', () => {
+    const merged = mergeConfig(defaults, { timeout: 123 })
+    expect(merged.timeout).toBe(123)
+  })
+})
